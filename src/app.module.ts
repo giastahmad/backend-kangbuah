030f766a4dd5311cfde24e9c438e8e6275a2637a
giastahmad <giast360@gmail.com>
2025-09-17 17:45:43 +0700

    add: create feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { ChatModule } from './chat/chat.module';
+
+const dbPort = process.env.DB_PORT;
+
+if(!dbPort){
+  throw new Error("Variable DB PORT not found");
+}
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(dbPort),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // HANYA untuk development, otomatis buat tabel
+    }),
+    AuthModule,
+    ProductsModule,
+    OrdersModule,
+    ChatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
