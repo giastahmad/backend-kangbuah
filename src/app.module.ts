06113269e1ac10ce0f49c98b2d7c99e46ed73a0a
risya22008 <risyaannisachairyah1411@gmail.com>
2025-09-18 10:03:45 +0700

    feat: integrate Supabase with NestJS application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { ChatModule } from './chat/chat.module';
-
-const dbPort = process.env.DB_PORT;
-
-if(!dbPort){
-  throw new Error("Variable DB PORT not found");
-}
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(dbPort),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // HANYA untuk development, otomatis buat tabel
-    }),
+    SupabaseModule, 
     AuthModule,
     ProductsModule,
     OrdersModule,
