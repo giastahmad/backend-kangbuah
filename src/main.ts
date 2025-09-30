09179a9014f3ea99525148bd3cdcde53864ad979
giastahmad <giast360@gmail.com>
2025-09-30 13:35:27 +0700

    give CORS Permission to FE URL (dev)

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,13 @@ async function bootstrap() {
   });
 
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,POST,PATCH,DELETE',
+    credential: true
+  })
+
   app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
