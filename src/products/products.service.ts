13f234771503d95a4fffb847ed49d647b2c4f744
giastahmad <giast360@gmail.com>
2025-10-26 21:19:22 +0700

    add stock logic

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product, ProductStatus, ProductType } from './entities/product.entity';
-import { In, Like, Repository } from 'typeorm';
+import { In, Like, Not, Repository } from 'typeorm';
 import { CreateProductDto } from './dto/createProduct.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { DeleteResult } from 'typeorm/browser';
@@ -71,77 +71,85 @@ export class ProductsService {
     const newProduct = this.productsRepository.create({
       ...createProductDto,
       product_id: uuidv4(),
-      image_url: imageUrls
+      image_url: imageUrls,
     });
 
     if (newProduct.stock == 0) {
       newProduct.status = ProductStatus.STOK_HABIS;
+    } else if (newProduct.stock > 0 && newProduct.stock <= 10) {
+      newProduct.status = ProductStatus.STOK_MENIPIS;
     }
 
     return this.productsRepository.save(newProduct);
   }
 
   async findAll(option: {
-  page: number;
-  limit: number;
-  status?: ProductStatus[];
-  type?: ProductType;
-  isAdmin: boolean;
-  search?: string;  
-  sortBy?: string; 
-  order?: 'ASC' | 'DESC'; 
-}) {
-  const take = option.limit || 10;
-  const skip = (option.page - 1) * take;
-  const where: any = {};
+    page: number;
+    limit: number;
+    status?: ProductStatus[];
+    type?: ProductType;
+    isAdmin: boolean;
+    search?: string;
+    sortBy?: string;
+    order?: 'ASC' | 'DESC';
+  }) {
+    const take = option.limit || 10;
+    const skip = (option.page - 1) * take;
+    const where: any = {};
 
-  
-  if (!option.isAdmin) {
-    where.status = ProductStatus.TERSEDIA;
-  } else if (option.isAdmin && option.status) {
-    where.status = In(Array.isArray(option.status) ? option.status : [option.status]);
+    if (!option.isAdmin) {
+      where.status = Not(
+        In([ProductStatus.TIDAK_AKTIF, ProductStatus.STOK_HABIS]),
+      );
+    } else if (option.isAdmin && option.status) {
+      where.status = In(
+        Array.isArray(option.status) ? option.status : [option.status],
+      );
+    }
+
+    if (option.type) {
+      where.type = option.type;
+    }
+
+    if (option.search) {
+      where.name = Like(`%${option.search}%`);
+    }
+
+    const orderOptions: any = {};
+    if (option.sortBy) {
+      orderOptions[option.sortBy] = option.order || 'ASC';
+    } else {
+      orderOptions['name'] = 'ASC';
+    }
+
+    const [result, total] = await this.productsRepository.findAndCount({
+      take: take,
+      skip: skip,
+      where: where,
+      order: orderOptions,
+    });
+
+    return {
+      data: result,
+      page: option.page,
+      max_page: Math.ceil(total / take),
+      total: total,
+    };
   }
-
-  if (option.type) {
-    where.type = option.type;
-  }
-
-  if (option.search) {
-    where.name = Like(`%${option.search}%`);
-  }
-
- 
-  const orderOptions: any = {};
-  if (option.sortBy) {
-    orderOptions[option.sortBy] = option.order || 'ASC';
-  } else {
-    orderOptions['name'] = 'ASC'; 
-  }
-
-  const [result, total] = await this.productsRepository.findAndCount({
-    take: take,
-    skip: skip,
-    where: where,
-    order: orderOptions,
-  });
-
-  return {
-    data: result, 
-    page: option.page,
-    max_page: Math.ceil(total / take),
-    total: total,
-  };
-}
 
   async delete(id: string) {
-    const product = await this.productsRepository.findOneBy({product_id: id});
+    const product = await this.productsRepository.findOneBy({ product_id: id });
     const supabaseClient = this.supabaseService.getClient();
 
-    if(product){
-      const imagesToDelete: string[] = product.image_url
-      if(imagesToDelete && imagesToDelete.length > 0){
-        const fileNameToDelete = imagesToDelete.map(url => url.split('/').pop()!);
-        await supabaseClient.storage.from('product-images').remove(fileNameToDelete);
+    if (product) {
+      const imagesToDelete: string[] = product.image_url;
+      if (imagesToDelete && imagesToDelete.length > 0) {
+        const fileNameToDelete = imagesToDelete.map(
+          (url) => url.split('/').pop()!,
+        );
+        await supabaseClient.storage
+          .from('product-images')
+          .remove(fileNameToDelete);
       }
     }
 
@@ -152,7 +160,11 @@ export class ProductsService {
     }
   }
 
-  async update(id: string, updateProductDto: UpdateProductDto, files: Array<Express.Multer.File>) {
+  async update(
+    id: string,
+    updateProductDto: UpdateProductDto,
+    files: Array<Express.Multer.File>,
+  ) {
     const product = await this.productsRepository.findOneBy({ product_id: id });
 
     if (!product) {
@@ -160,27 +172,44 @@ export class ProductsService {
     }
 
     const supabaseClient = this.supabaseService.getClient();
-
     const oldImageUrls = product.image_url || [];
-    const existingImageUrls = updateProductDto.existing_image_url || [];
-    const urlToDelete = oldImageUrls.filter(url => !existingImageUrls.includes(url))
+    const dtoExistingUrls = updateProductDto.existing_image_url;
 
-    if(urlToDelete.length > 0){
-      const fileNameToDelete = urlToDelete.map(url => url.split('/').pop()!);
-      await supabaseClient.storage.from('product-images').remove(fileNameToDelete)
+    const existingImageUrls =
+      dtoExistingUrls !== undefined
+        ? dtoExistingUrls.length === 1 && dtoExistingUrls[0] === ''
+          ? []
+          : dtoExistingUrls
+        : product.image_url || [];
+
+    const urlToDelete = oldImageUrls.filter(
+      (url) => !existingImageUrls.includes(url),
+    );
+
+    if (urlToDelete.length > 0) {
+      const fileNameToDelete = urlToDelete.map((url) => url.split('/').pop()!);
+      await supabaseClient.storage
+        .from('product-images')
+        .remove(fileNameToDelete);
     }
 
     const newImages: string[] = [];
-    if(files && files.length > 0){
-      const uploadPromise = files.map(file => {
+    if (files && files.length > 0) {
+      const uploadPromise = files.map((file) => {
         const newFileName = `${Date.now()}-${file.originalname}`;
-        return supabaseClient.storage.from('product-images').upload(newFileName, file.buffer, {contentType: file.mimetype});
+        return supabaseClient.storage
+          .from('product-images')
+          .upload(newFileName, file.buffer, { contentType: file.mimetype });
       });
       const uploadResult = await Promise.all(uploadPromise);
 
-      uploadResult.forEach(result => {
-        if(result.data){
-          const {data: {publicUrl}} = supabaseClient.storage.from('product-images').getPublicUrl(result.data.path);
+      uploadResult.forEach((result) => {
+        if (result.data) {
+          const {
+            data: { publicUrl },
+          } = supabaseClient.storage
+            .from('product-images')
+            .getPublicUrl(result.data.path);
           newImages.push(publicUrl);
         }
       });
@@ -194,6 +223,8 @@ export class ProductsService {
     if (product.status !== ProductStatus.TIDAK_AKTIF) {
       if (product.stock <= 0) {
         product.status = ProductStatus.STOK_HABIS;
+      } else if (product.stock > 0 && product.stock <= 10) {
+        product.status = ProductStatus.STOK_MENIPIS;
       } else {
         product.status = ProductStatus.TERSEDIA;
       }
